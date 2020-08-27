ae38531f764d0e185ec3d87ee8723c5a59ec720b
jasonwilliams-jcu <jason.williams1@my.jcu.edu.au>
2020-08-27 15:02:45 +1000

    week 3 prac

---
+++ prac_03/password_check.py
@@ -0,0 +1,13 @@
+MINIMUM_LENGTH = 4
+
+
+def version_1():
+    password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
+    while len(password) < MINIMUM_LENGTH:
+        password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
+
+    print('*' * len(password))
+
+
+version_1()
+
